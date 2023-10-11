88f9d8ddd34ce160de72008779b6dbd9476ea992
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-10-11 10:14:17 +0300

    result commit

--- src/js/Character.js
+++ src/js/Character.js
@@ -19,5 +19,27 @@ export default class Character {
 
     this.name = name;
     this.type = type;
+
+    this.health = 100;
+    this.level = 1;
+    this.attack = 0;
+    this.defence = 0;
+  }
+
+  levelUp() {
+    if (this.health === 0) {
+      this.level += 1;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+      this.health = 100;
+    } else {
+      throw new Error('Нельзя повысить левел умершего');
+    }
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
   }
 }
